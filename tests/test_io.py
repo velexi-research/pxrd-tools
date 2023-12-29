7c219c560962ef44230dc223cc47cf3a5c6e3f9e
Kevin Chu <kevin@velexi.com>
2023-12-29 12:04:32 -0800

    Add read_prn(). Update unit tests.

--- tests/test_io.py
+++ tests/test_io.py
@@ -30,8 +30,8 @@ class test_pxrd_tools_io(unittest.TestCase):
         """
         Prepare for test.
         """
-        self.test_data_file = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "data", "test-data.csv")
+        self.test_data_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "data")
         )
 
     def tearDown(self):
@@ -63,21 +63,66 @@ class test_pxrd_tools_io(unittest.TestCase):
 
         # ------ path is a str
 
-        path = self.test_data_file
-        data = pxrd_tools.io.read_csv(path, delimiter=r"\s+")
+        path = os.path.join(self.test_data_dir, "test-data.csv")
+        data = pxrd_tools.io.read_csv(path)
 
         # Check results
         assert isinstance(data, DataFrame)
         assert list(data.columns) == pxrd_tools.io._PXRD_DATAFRAME_COLUMNS
         assert len(data) == 7251
+        assert data.isna().sum().sum() == 0
 
         # ------ path is a Path
 
-        path = Path(self.test_data_file)
+        path = Path(os.path.join(self.test_data_dir, "test-data.csv"))
         assert isinstance(path, Path)
-        data = pxrd_tools.io.read_csv(path, delimiter=r"\s+")
+        data = pxrd_tools.io.read_csv(path)
 
         # Check results
         assert isinstance(data, DataFrame)
         assert list(data.columns) == pxrd_tools.io._PXRD_DATAFRAME_COLUMNS
         assert len(data) == 7251
+        assert data.isna().sum().sum() == 0
+
+    @staticmethod
+    def test_read_prn_arg_checks():
+        """
+        Test argument checks for `read_prn()`.
+        """
+        # --- Exercise functionality and check results
+
+        path = "invalid/path"
+
+        with pytest.raises(ValueError) as exception_info:
+            pxrd_tools.io.read_prn(path)
+
+        assert f"Data file '{path}' not found" in str(exception_info)
+
+    def test_read_prn(self):
+        """
+        Test `read_prn()`.
+        """
+        # --- Exercise functionality and check results
+
+        # ------ path is a str
+
+        path = os.path.join(self.test_data_dir, "test-data.prn")
+        data = pxrd_tools.io.read_prn(path)
+
+        # Check results
+        assert isinstance(data, DataFrame)
+        assert list(data.columns) == pxrd_tools.io._PXRD_DATAFRAME_COLUMNS
+        assert len(data) == 7251
+        assert data.isna().sum().sum() == 0
+
+        # ------ path is a Path
+
+        path = Path(os.path.join(self.test_data_dir, "test-data.prn"))
+        assert isinstance(path, Path)
+        data = pxrd_tools.io.read_prn(path)
+
+        # Check results
+        assert isinstance(data, DataFrame)
+        assert list(data.columns) == pxrd_tools.io._PXRD_DATAFRAME_COLUMNS
+        assert len(data) == 7251
+        assert data.isna().sum().sum() == 0
